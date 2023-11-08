07b55c01f970d2231865305febfad6778af3c3cc
Cap Bao Dien <diencb@innotech-vn.com>
2023-11-08 16:18:13 +0700

    Initial project trello

---
+++ components/theme-registry/theme-registry.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import NextAppDirEmotionCacheProvider from "./emotion-cache";
+import { theme } from "@/utils";
+
+export default function ThemeRegistry({ children }: { children: ReactNode }) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
