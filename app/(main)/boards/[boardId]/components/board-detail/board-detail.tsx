5ed4a219f8480fbfe36f166df7dc6069f7ba1f8b
Dien Cap <baodien1405@gmail.com>
2024-12-05 08:18:28 +0700

    refactor: Move logic board detail to useEffect

--- app/(main)/boards/[boardId]/components/board-detail/board-detail.tsx
+++ app/(main)/boards/[boardId]/components/board-detail/board-detail.tsx
@@ -9,6 +9,7 @@ import { BoardBar } from '@/app/(main)/boards/[boardId]/components/board-bar'
 import { BoardContent } from '@/app/(main)/boards/[boardId]/components/board-content'
 import SpinnerLoading from '@/components/spinner-loading'
 import { ActiveCardModal } from '@/app/(main)/boards/[boardId]/components/active-card-modal'
+import { useEffect } from 'react'
 
 interface BoardDetailProps {
   boardId: string
@@ -19,20 +20,24 @@ export function BoardDetail({ boardId }: BoardDetailProps) {
   const { data, isLoading } = useBoardDetailsQuery(boardId)
   const board = data?.metadata
 
+  useEffect(() => {
+    if (!board) return
+
+    board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
+
+    board.columns.forEach((column) => {
+      if (column.cards.length === 0) {
+        column.cards = [generatePlaceholderCard(column)]
+        column.cardOrderIds = [generatePlaceholderCard(column)._id]
+      } else {
+        column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+      }
+    })
+  }, [board])
+
   if (!board || isLoading) {
     return <SpinnerLoading caption="Loading board..." />
   }
-
-  board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
-
-  board.columns.forEach((column) => {
-    if (column.cards.length === 0) {
-      column.cards = [generatePlaceholderCard(column)]
-      column.cardOrderIds = [generatePlaceholderCard(column)._id]
-    } else {
-      column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
-    }
-  })
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
