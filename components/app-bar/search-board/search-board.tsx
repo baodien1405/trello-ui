d8cec8c21e0de40b32203e89dfe39fda9f2184c9
Dien Cap <baodien1405@gmail.com>
2025-01-01 09:10:11 +0700

    feat: Support search title of board

--- components/app-bar/search-board/search-board.tsx
+++ components/app-bar/search-board/search-board.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { SyntheticEvent, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import TextField from '@mui/material/TextField'
@@ -9,7 +9,10 @@ import CircularProgress from '@mui/material/CircularProgress'
 import InputAdornment from '@mui/material/InputAdornment'
 import SearchIcon from '@mui/icons-material/Search'
 import { debounce } from '@mui/material'
+
 import { useBoardListQuery } from '@/hooks'
+import { RoutePath } from '@/constants'
+import { Board } from '@/models'
 
 export function SearchBoard() {
   const router = useRouter()
@@ -29,18 +32,12 @@ export function SearchBoard() {
       ...prevState,
       'q[title]': searchValue
     }))
-
-    // Dùng createSearchParams của react-router-dom để tạo một cái searchPath chuẩn với q[title] để gọi lên API
-    // const searchPath = `?${createSearchParams({ 'q[title]': searchValue })}`
-    // console.log(searchPath)
-
-    // Gọi API...
   }, 350)
 
-  // Khi chúng ta select chọn một cái board cụ thể thì sẽ điều hướng tới board đó luôn
-  const handleSelectedBoard = (event: any, selectedBoard: any) => {
-    // Phải kiểm tra nếu tồn tại một cái board cụ thể được select thì mới gọi điều hướng - navigate
-    console.log(selectedBoard)
+  const handleSelectedBoard = (_event: SyntheticEvent, selectedBoard: Board | null) => {
+    if (selectedBoard) {
+      router.push(`${RoutePath.BOARDS}/${selectedBoard._id}`)
+    }
   }
 
   return (
@@ -48,15 +45,14 @@ export function SearchBoard() {
       sx={{ width: 220 }}
       id="asynchronous-search-board"
       noOptionsText={boardList.length === 0 ? 'No board found!' : 'Type to search board...'}
-      // open={open}
-      // onOpen={() => setOpen(true)}
-      // onClose={() => {
-      //   setOpen(false)
-      // }}
+      open={open}
+      onOpen={() => setOpen(true)}
+      onClose={() => {
+        setOpen(false)
+      }}
       getOptionLabel={(option) => {
         return option.title
       }}
-      // Options của Autocomplete nó cần đầu vào là 1 Array, mà boards của chúng ta ban đầu cần cho null để làm cái noOptionsText ở trên nên đoạn này cần thêm cái || [] vào
       options={boardList}
       isOptionEqualToValue={(option, value) => {
         return option._id === value._id
@@ -70,23 +66,26 @@ export function SearchBoard() {
           label="Type to search..."
           placeholder="Search..."
           size="small"
-          // slotProps={{
-          //   ...params.InputProps,
-          //   input: {
-          //     startAdornment: (
-          //       <InputAdornment position="start">
-          //         <SearchIcon sx={{ color: 'white' }} />
-          //       </InputAdornment>
-          //     ),
-          //     endAdornment: (
-          //       <>
-          //         <InputAdornment position="end">
-          //           <CircularProgress sx={{ color: 'white' }} size={20} />
-          //         </InputAdornment>
-          //       </>
-          //     )
-          //   }
-          // }}
+          slotProps={{
+            input: {
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon sx={{ color: 'white' }} />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <>
+                  <InputAdornment position="end">
+                    {boardListQuery.isLoading ? (
+                      <CircularProgress sx={{ color: 'white' }} size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </InputAdornment>
+                </>
+              )
+            }
+          }}
           sx={{
             '& label': { color: 'white' },
             '& input': { color: 'white' },
